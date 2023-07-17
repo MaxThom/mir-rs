10b192fe614fdcd015ab2ee84166d87c26f449c8
maxthom <maxthomassin@hotmail.com>
2023-07-17 21:14:44 +1200

    started dizer

---
+++ src/dv-dizer/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+    // Builder pattern
+    // - register config file
+    //   - mir config file
+    //   - users config file
+    // - pass receive twin desired properties updates function handler
+    // - pass receive commands function handler
+    // Sending tlmt
+    // - implement send twin reported properties
+    // - implement send twin meta properties
+    // - implement send telemetry
+    // - implement logging using log interface
+    // - implement send metrics
+}
